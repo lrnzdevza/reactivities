1a09d8eade219b6f9f9bdf3bc23e469e89f4eedb
unknown <Lrnzdevza@gmail.com>
2020-04-16 22:31:51 +0200

    End of Section 7

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,20 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
-import { resolveAny } from 'dns';
 import { IActivity } from '../models/activity';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-//Adding in delay to the responses...
-const sleep = (ms: number) => (response: AxiosResponse) =>
+const sleep = (ms: number) => (response: AxiosResponse) => 
     new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
 
 const requests = {
     get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody)
+    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody) 
 };
 
 const Activities = {
